95e0cf0c2500d0a772f3178a563d1d941ec994e8
Dmytro Alieksieienko <dmytro.alieks.at@gmail.com>
2026-04-10 10:24:55 +0200

    Conference database and gitignore

---
+++ Conference-DB/Referenten.sql
@@ -0,0 +1,2 @@
+INSERT INTO conference_db.Referenten (id, name, email, organisation) VALUES (1, 'Dima', 'test@gmail.com', 'SZF');
+INSERT INTO conference_db.Referenten (id, name, email, organisation) VALUES (2, 'Volodia', 'volodia@gmail.com', 'SZF');
---
+++ Conference-DB/Teilnehmer_nach_Themen.sql
@@ -0,0 +1,4 @@
+INSERT INTO conference_db.Teilnehmer_nach_Themen (teilnehmer_id, themen_id) VALUES (3, 1);
+INSERT INTO conference_db.Teilnehmer_nach_Themen (teilnehmer_id, themen_id) VALUES (4, 2);
+INSERT INTO conference_db.Teilnehmer_nach_Themen (teilnehmer_id, themen_id) VALUES (2, 3);
+INSERT INTO conference_db.Teilnehmer_nach_Themen (teilnehmer_id, themen_id) VALUES (1, 4);
