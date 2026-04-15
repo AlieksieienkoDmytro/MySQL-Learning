02f84c40a5006fb9e3769acbc171f2a7fdee5175
Dmytro Alieksieienko <dmytro.alieks.at@gmail.com>
2026-04-15 10:51:57 +0200

    Upload Create DB SQL Script

---
+++ LE-07-01/create DB.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS bibliothek;
+USE bibliothek;
+
+CREATE TABLE Verlag (
+                        verlag_id INT AUTO_INCREMENT PRIMARY KEY,
+                        verlag VARCHAR(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE Fachbuch (
+                          fachbuch_id INT AUTO_INCREMENT PRIMARY KEY,
+                          ISBN VARCHAR(20) NOT NULL UNIQUE,
+                          titel VARCHAR(255) NOT NULL,
+                          verlag_id INT,
+                          FOREIGN KEY (verlag_id) REFERENCES Verlag(verlag_id)
+);
+
+CREATE TABLE Fachbereich (
+                             fachbereich_id INT AUTO_INCREMENT PRIMARY KEY,
+                             ISBN VARCHAR(20) NOT NULL UNIQUE,
+                             titel VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Fachbereich_Fachbuch (
+                                      fachbereich_id INT,
+                                      fachbuch_id INT,
+                                      FOREIGN KEY (fachbereich_id) REFERENCES Fachbereich(fachbereich_id),
+                                      FOREIGN KEY (fachbuch_id) REFERENCES Fachbuch(fachbuch_id)
+);
